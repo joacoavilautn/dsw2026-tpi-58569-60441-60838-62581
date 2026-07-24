ed927decc9d2670247cc909e15bb831db1f0b80a
Nacho Ortega <Miguel.Ortega@alu.frt.utn.edu.ar>
2026-07-24 19:05:20 -0300

    configuracion fluentAPI

--- Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
+++ Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Dsw2026Tpi.Data;
 
 public class Dsw2026TpiDbContext: DbContext
 {
+    public DbSet<Speciality> Specialties { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+
     public Dsw2026TpiDbContext(DbContextOptions<Dsw2026TpiDbContext> options):
         base(options)
     {
