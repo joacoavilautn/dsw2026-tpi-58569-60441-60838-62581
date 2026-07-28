903f5ed833d1aab08c96f799d744c88e1bb48c67
SantiQuiroga76 <santiquiroga.na@gmail.com>
2026-07-28 12:22:14 -0300

    Creacion y configuracion AppointmentService y registro de servicio

---
+++ Dsw2026Tpi.Application/Services/AppointmentService.cs
@@ -0,0 +1,84 @@
+﻿using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.CrossCutting.Models;
+using Dsw2026Tpi.Data;
+using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Core;
+
+namespace Dsw2026Tpi.Application.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly Dsw2026TpiDbContext _context;
+        private readonly ILogger<AppointmentService> _logger;
+
+        public AppointmentService(Dsw2026TpiDbContext dbContext, ILogger<AppointmentService> logger)
+        {
+            _context = dbContext;
+            _logger = logger;
+        }
+        public async Task<AppointmentModel.Response> BookAppointmentAsync(AppointmentModel.Request request, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation($"Iniciando reserva de un turno para el slot {request.AvailabilityId} y paciente DNI {request.Patient.Dni}");
+
+            var patient = await _context.Set<Patient>().FirstOrDefaultAsync(p => p.Dni == request.Patient.Dni.ToString() && !p.Deleted, cancellationToken);
+            if(patient == null) throw new EntityNotFoundException("Patient");
+
+            var slot = await _context.Set<AvailabilitySlot>().FirstOrDefaultAsync(s => s.Id == request.AvailabilityId && !s.Deleted, cancellationToken);
+            if(slot == null) throw new EntityNotFoundException("AvailabilitySlot");
+
+            if(slot.Status != SlotStatus.AVAILABLE) throw new ConflictException("APPOINTMENT_CONFLICT", "El turno ya fue reservado o bloqueado");
+
+            var slotFullDateTime = slot.SlotDate.Date.Add(slot.StartTime);
+            if (slotFullDateTime < DateTime.UtcNow) throw new BusinessRuleException("No se pueden reservar turnos en fechas pasadas.", "PAST_DATE_NOT_ALLOWED");
+
+            var appointment = new Appointment(request.DoctorId, slot.Id, patient.Id, request.Reason);
+            slot.Reserve();
+
+            _context.Appointments.Add(appointment);
+
+            try
+            {
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch(DbUpdateConcurrencyException ex)
+            {
+                _logger.LogWarning(ex, $"Doble reserva detectada en el slot {slot.Id}");
+                throw new ConflictException("APPOINTMENT_CONFLICT", "El turno ya esta reservado");
+            }
+
+            return new AppointmentModel.Response(
+                appointment.Id,
+                appointment.DoctorId,
+                appointment.AvailabilitySlotId,
+                appointment.Status.ToString()
+                );
+        }
+
+        public async Task<bool> CancelAppointmentAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation($"Cancelando turno {id}");
+
+            var appointment = await _context.Appointments.FirstOrDefaultAsync(a => a.Id == id, cancellationToken);
+            if (appointment == null) throw new EntityNotFoundException("Appointment");
+
+            appointment.Cancel(DateTime.UtcNow);
+
+            var slot = await _context.Set<AvailabilitySlot>().FirstOrDefaultAsync(s => s.Id == appointment.AvailabilitySlotId, cancellationToken);
+
+            if(slot != null)
+            {
+                slot.Release();
+            }
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
