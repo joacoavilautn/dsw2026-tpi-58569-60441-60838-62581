9b53def58f4dd5e7e0ddce4761f4b2e78c8ec3ae
SantiQuiroga76 <santiquiroga.na@gmail.com>
2026-07-30 17:25:53 -0300

    Coreccion capa Api - AppointmentsController

--- Dsw2026Tpi.Api/Controllers/AppointmentsController.cs
+++ Dsw2026Tpi.Api/Controllers/AppointmentsController.cs
@@ -3,11 +3,13 @@ using Dsw2026Tpi.Application.Dtos;
 using Dsw2026Tpi.Application.Interfaces;
 using Dsw2026Tpi.Application.Services;
 using System.Security.Cryptography.Pkcs;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Dsw2026Tpi.Api.Controllers
 {
     [ApiController]
     [Route("api/appointments")]
+    [Authorize]
     public class AppointmentsController : ControllerBase
     {
         private readonly IAppointmentService _appointmentService;
@@ -17,18 +19,33 @@ namespace Dsw2026Tpi.Api.Controllers
             _appointmentService = appointmentService;
         }
 
+        //Solicitar/Reservar turno medico disponible.
         [HttpPost]
+        [Authorize(Roles = "PACIENTE")]
         public async Task<IActionResult> BookAppointment([FromBody] AppointmentModel.Request request, CancellationToken cancellationToken)
         {
-            var result = await _appointmentService.BookAppointmentAsync(request, cancellationToken);
-            return Ok(result);
+            var response = await _appointmentService.BookAppointmentAsync(request, cancellationToken);
+            return Ok(response);
         }
 
+        //Cancelar turno reservado
         [HttpDelete("{id:guid}")]
+        [Authorize(Roles = "PACIENTE")]
         public async Task<IActionResult> CancelAppointment(Guid id, CancellationToken cancellationToken)
         {
             await _appointmentService.CancelAppointmentAsync(id, cancellationToken);
             return NoContent();
+        }
+
+        //Ver turnos activos del paciente (solo aquellos en estado BOOKED)
+        [HttpGet("patient")]
+        [Authorize(Roles = "PACIENTE, ADMINISTRADOR")]
+        public async Task<IActionResult> GetActiveByPatient([FromQuery] string dni, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(dni)) return BadRequest(new { Message = "El dni es obligatorio para realizar la busqueda de turnos." });
+
+            var appointments = await _appointmentService.GetActiveAppointmentsByPatientDniAsync(dni, cancellationToken);
+            return Ok(appointments);
         }
     }
 }
